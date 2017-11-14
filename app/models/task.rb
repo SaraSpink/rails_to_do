127ed959a2d7e2618641e88ea4fde8f4741b8515
Qudsia Ahmad and Sara Spink <qudsia@>
2017-11-13 16:29:22 -0800

    method to show completed tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,17 @@
 class Task < ActiveRecord::Base
   belongs_to :list
-
   validates :description, :presence => true
-  
+
+
+  scope(:is_done, -> do
+    where({:done => true})
+  end)
+
+
+
+
+
+  # def task_not_done
+  #   self.done = false
+  # end
 end
